b412a2482f09d4754a49a30462af6d7f588b4d29
Yauheni Markevich <2879105@gmail.com>
2020-12-10 21:35:33 +0300

    HomeTask for Master

--- src/main/java/com/app/web/MasterWeb.java
+++ src/main/java/com/app/web/MasterWeb.java
@@ -1,13 +1,13 @@
 package com.app.web;
 
+import com.app.model.entity.PersistableEntityImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.app.model.entity.MasterEntity;
 import com.app.service.MasterService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/master")
@@ -15,12 +15,34 @@ public class MasterWeb
 {
 	private MasterService masterService;
 
-	@PostMapping("/add-master")
+	/*@PostMapping("/add-master")
 	public void addUser(@RequestBody MasterEntity userEntity)
 	{
 		masterService.addUser(userEntity);
-	}
+           	}
+*/
 
+
+	@PostMapping("/save-master")
+	public void saveMaster(@RequestBody MasterEntity userEntity)
+	{
+		masterService.saveMaster(userEntity);
+	}
+	@DeleteMapping("/delete-master")
+	public void deleteMaster(@RequestBody MasterEntity userEntity)
+	{
+		masterService.deleteMaster(userEntity);
+	}
+	@GetMapping ("/getById-master/{id}")
+	public MasterEntity getMasterById(@PathVariable long id)
+	{
+		return masterService.getMasterById(id);
+	}
+	@GetMapping ("/findAll-master")
+	public List<MasterEntity> findAllMasters()
+	{
+		return masterService.findAllMasters();
+	}
 	@Autowired
 	public void setMasterService(MasterService masterService)
 	{
